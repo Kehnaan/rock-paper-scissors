6dbf22038af7a76420a138a289aef1b35f0c7171
Kehnaan <kehnaan10@gmail.com>
2024-11-05 07:16:35 -0500

    Added function for human choice

--- script.js
+++ script.js
@@ -12,6 +12,21 @@ function getComputerChoice () {
 
 }
 
-console.log(getComputerChoice());
+function getHumanChoice () {
+    let userInput = prompt("Enter your choice (rock, paper or scissors): ")
+
+    let userChoice = userInput.toLowerCase();
+
+    //validate user choice
+    if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+        return userChoice;
+    } else {
+        console.log("Invalid choice! Enter rock, paper, or scissors.");
+        return getHumanChoice;
+    }
+}
 
 
+// Test the function
+let humanChoice = getHumanChoice();
+console.log("You chose: " + humanChoice);
